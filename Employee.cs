cd2b670c8da2b308b5bd56e8b7f8bcd961a03279
Gayathri-RP <68377760+Gayathri-RP@users.noreply.github.com>
2020-08-10 18:00:07 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+
+namespace LinqandLambaExpression
+{
+    public class Employee
+    {
+        public int EmployeeID{get;set;}
+        public string EmployeeName {get;set;}
+        public string Gender {get; set;}
+        public string Role {get;set;}
+        public string department {get;set;}
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,23 +1,44 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
 
-namespace SingletonPractice
+
+
+namespace LinqandLambaExpression
 {
     class Program
     {
         static void Main(string[] args)
         {
-            SingletonClass s1 = SingletonClass.GetInstance();
-            SingletonClass s2 = SingletonClass.GetInstance();
+             
+            List<Employee> employees = new List<Employee> ()
+            {
+                new Employee {EmployeeID = 101 , EmployeeName ="Gayathri",Gender = "F", Role = "PAT", department = "dotnet"},
+                new Employee {EmployeeID = 102 , EmployeeName ="Keerthika",Gender = "F", Role = "PAT", department = "dotnet"},
+                new Employee {EmployeeID = 103 , EmployeeName ="Udaya",Gender = "M", Role = "PA", department = "Java"},
+                new Employee {EmployeeID = 104 , EmployeeName ="Gokul",Gender = "M", Role = "A", department = "Dotnet"},
+                new Employee {EmployeeID = 105 , EmployeeName ="Nathan",Gender = "M", Role = "SA", department = "Fullstack"},
+                new Employee {EmployeeID = 106 , EmployeeName ="Sowmiya",Gender = "F", Role = "PAT", department = "Java"},
+                new Employee {EmployeeID = 107 , EmployeeName ="ABC",Gender = "M", Role = "SA", department = "dotnet"},
 
-            if (s1 == s2)
-            {
-                Console.WriteLine("Singleton works, both variables contain the same instance.");
+
+            };
+            var Names = employees.Select(x=>x.EmployeeName);
+            foreach (var employee in Names)
+                       {
+             Console.WriteLine(employee);
             }
-            else
-            {
-                Console.WriteLine("Singleton failed, variables contain different instances.");
-            }
-            Console.ReadLine();
+            Action<int> act = EmployeeID => Console.WriteLine(EmployeeID);
+            act(108);
+            Func<string,string,int> totalLength = (s1, s2) => s1.Length + s2.Length;
+            int total = totalLength ("hi", "there"); 
+            Console.WriteLine(total);
+   }  
+            
+            
+            
         }
     }
-}
+
+ 
